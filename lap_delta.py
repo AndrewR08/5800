361ce3466cb18ed963419048a3d5ab31e8a138c8
AndrewR08 <reevesat@appstate.edu>
2023-03-28 23:04:32 -0400

    added way to get driver gap (bad)

--- lap_delta.py
+++ lap_delta.py
@@ -3,12 +3,12 @@ import pandas as pd
 from fastf1 import plotting
 from fastf1 import utils
 from matplotlib import pyplot as plt
-from datetime import datetime, timedelta
 
 # options for easier readability on df print
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
-#pd.set_option("expand_frame_repr", False)
+pd.set_option('display.max_rows', None)
+pd.set_option("expand_frame_repr", False)
 
 
 def cache(pc):
@@ -18,6 +18,10 @@ def cache(pc):
     else:
         # location of cahce for mac
         ff1.Cache.enable_cache('/Users/andrewreeves/Documents/ASU/fastf1')
+
+
+def sub(a, b):
+    return a-b
 
 
 def custom_delta(ref_lap, com_lap):
@@ -34,26 +38,28 @@ def plot_delta(year, race, d1, d2, lap_num=8):
     d1_lap = d1_laps[d1_laps['LapNumber'] == lap_num].iloc[0]
     d2_laps = session.laps.pick_driver(d2)
     d2_lap = d2_laps[d2_laps['LapNumber'] == lap_num].iloc[0]
-    print("--d1 lap--\n", d1_lap)
-    print("\n--d2 lap--\n", d2_lap)
+    # print("--d1 lap--\n", d1_lap)
+    # print("\n--d2 lap--\n", d2_lap)
 
     d1_name = d1_lap.Driver
-    d2_name = d2_lap.Drivera
+    d2_name = d2_lap.Driver
 
     delta_time, ref_tel, compare_tel = utils.delta_time(d1_lap, d2_lap)
 
-    #print(ref_tel)
-    #print(compare_tel)
+    print(ref_tel[['Time', 'Distance']])
+    print(compare_tel[['Time', 'Distance']])
+    print(delta_time)
 
     actual_gap = str(abs(d1_lap['LapTime'] - d2_lap['LapTime']))[14:]
 
     fig, ax = plt.subplots()
     # use telemetry returned by .delta_time for best accuracy,
     # this ensure the same applied interpolation and resampling
-    ax.plot(ref_tel['Distance'], delta_time, '--', color='white')
-    ax.set_ylabel("<-- " + d2_name + " ahead | " + d1_name + " ahead -->")
+    ax.plot(delta_time, ref_tel['Distance'], '--', color='white')
+    ax.set_xlabel("<-- " + d2_name + " ahead | " + d1_name + " ahead -->")
+    ax.set_ylabel("Distance")
     plt.title('Lap Time Delta (Actual= ' + actual_gap + ' s)')
-    plt.show()
+    #plt.show()
 
 
 def time_dist(year, race, d1, d2, lap_num=8):
@@ -104,7 +110,7 @@ def time_dist_all(year, race, drivers, lap_num=8):
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     ax.set_xlabel("Time (m:ss)")
     ax.set_ylabel("Distance (m)")
-    #ax.legend(['NOR', 'LEC'])
+    # ax.legend(['NOR', 'LEC'])
     plt.title("Lap Time vs Distance (Lap " + str(lap_num) + ")")
     plt.tight_layout()
     plt.show()
@@ -134,27 +140,27 @@ def time_dist_race_two(year, race, drivers, num_laps=0):
     # when driver 2 has more total laps than driver 2 (or equal laps)
     if len(d1_laps) <= len(d2_laps):
         for i in range(num_laps):
-            d1_lap = d1_laps[d1_laps['LapNumber'] == (i+1)].iloc[0]
+            d1_lap = d1_laps[d1_laps['LapNumber'] == (i + 1)].iloc[0]
             ref = d1_lap.get_car_data(interpolate_edges=True).add_distance()
             ref = ref[['Time', 'Distance']]
-            #print("---ref lap " + str(i+1) + "---\n", ref)
+            # print("---ref lap " + str(i+1) + "---\n", ref)
 
-            d2_lap = d2_laps[d2_laps['LapNumber'] == (i+1)].iloc[0]
+            d2_lap = d2_laps[d2_laps['LapNumber'] == (i + 1)].iloc[0]
             comp = d2_lap.get_car_data(interpolate_edges=True).add_distance()
             comp = comp[['Time', 'Distance']]
             # print("\n---comp lap " + str(i+1) + "---\n", comp)
 
             if i != 0:
                 ref_len = len(ref_laps)
-                last_ref_dist = ref_laps['Distance'].iloc[ref_len-1]
-                last_ref_time = ref_laps['Time'].iloc[ref_len-1]
+                last_ref_dist = ref_laps['Distance'].iloc[ref_len - 1]
+                last_ref_time = ref_laps['Time'].iloc[ref_len - 1]
                 print("lap " + str(i) + ", " + str(last_ref_time))
                 ref['Time'] = ref['Time'] + last_ref_time
                 ref['Distance'] = ref['Distance'] + last_ref_dist
 
                 comp_len = len(comp_laps)
-                last_comp_dist = comp_laps['Distance'].iloc[comp_len-1]
-                last_comp_time = comp_laps['Time'].iloc[comp_len-1]
+                last_comp_dist = comp_laps['Distance'].iloc[comp_len - 1]
+                last_comp_time = comp_laps['Time'].iloc[comp_len - 1]
                 comp['Time'] = comp['Time'] + last_comp_time
                 comp['Distance'] = comp['Distance'] + last_comp_dist
             """else:
@@ -201,35 +207,62 @@ def time_dist_race_all(year, race, drivers, num_laps=None):
         else:
             laps = num_laps
 
+        # use range(1, laps+1) to exclude lap 1
         for j in range(laps):
-            d_lap = d_laps[d_laps['LapNumber'] == (j+1)].iloc[0]
+            # for j in range(laps):
+            d_lap = d_laps[d_laps['LapNumber'] == (j + 1)].iloc[0]
             ref = d_lap.get_car_data(interpolate_edges=True).add_distance()
             ref = ref[['Time', 'Distance']]
 
-            if j != 0:
+            # use j != 1 to exclude lap 1
+            """if j != 1:
+                # if j != 0:
                 ref_len = len(laps_df)
-                last_ref_dist = laps_df['Distance'].iloc[ref_len-1]
-                last_ref_time = laps_df['Time'].iloc[ref_len-1]
+                last_ref_dist = laps_df['Distance'].iloc[ref_len - 1]
+                last_ref_time = laps_df['Time'].iloc[ref_len - 1]
                 ref['Time'] = ref['Time'] + last_ref_time
-                ref['Distance'] = ref['Distance'] + last_ref_dist
+                ref['Distance'] = ref['Distance'] + last_ref_dist"""
 
             ref['LapNumber'] = d_lap.LapNumber
             ref['Driver'] = d_laps.Driver.iloc[0]
             ref['DriverNumber'] = d_laps.DriverNumber.iloc[0]
+            ref['MaxDistance'] = max(ref['Distance'])
+
             laps_df = pd.concat([laps_df, ref])
+
+        # *** bad!! create separate columns for each driver distance
+        #dist_col = 'Distance_' + d_laps.Driver.iloc[0]
+        #laps_df[dist_col] = ref['Distance']
 
         ax.plot(laps_df['Time'].loc[laps_df['Driver'] == d_laps.Driver.iloc[0]],
                 laps_df['Distance'].loc[laps_df['Driver'] == d_laps.Driver.iloc[0]],
                 color=plotting.driver_color(d_laps.Driver.iloc[0]), label=d_laps.Driver.iloc[0])
 
-    # laps_df['Distance'] = laps_df['Distance'] / 1000
-    # print(laps_df)
-    # laps_df.to_csv('data/Monaco/MonacoTimeDist.csv', index=False)
+        #print(str(d_laps.Driver.iloc[0]) + " " + str(max(laps_df['Time'].loc[laps_df['DriverNumber'] == d])) +
+        #      " // " + str(max(laps_df['Distance'].loc[laps_df['DriverNumber'] == d])))
+
+    #print()
+    #print(min(laps_df['MaxDistance']))
+
+    """
+    # *** bad!!! - way to get distance gap and seperate columns ***
+    # leader driver number
+    leader_num = 16
+    #laps_df.dropna(inplace=True)
+    laps_df.drop(columns=['Distance'], inplace=True)
+    laps_df['DistanceGap'] = laps_df.apply(lambda row: (row['Distance_LEC'] - row['Distance_SAI']), axis=1)
+    laps_df['LeaderGap'] = 0
+    ax.plot(laps_df['Time'], laps_df['DistanceGap'],
+            color='red', label='SAI')
+    """
+
+    print(laps_df)
+    #laps_df.to_csv('data/Monaco/MonacoTD_LEC_SAI_L1_TEST.csv', index=False)
 
     ax.set_xlabel("Time (h:mm)")
     ax.set_ylabel("Distance (m)")
     ax.legend(loc='center right')
-    plt.title(race + " Lap Time vs Distance")
+    plt.title(race + " " + str(year) + " Lap Time vs Distance Gap")
     plt.show()
 
 
@@ -239,17 +272,16 @@ def main():
     drivers = ['11', '55', '1', '16', '63', '4', '14', '44', '77', '5', '10', '31', '3', '18', '6', '24', '22', '23',
                '47', '20']
 
-
     # drivers = ['VER', 'PER', 'LEC', 'SAI', 'RUS', 'HAM', 'NOR', 'RIC', 'ALO', 'OCO', 'BOT', 'ZHO', 'GAS', 'TSU',
     #            'STR', 'VET', 'MAG', 'MSC', 'ALB', 'LAT']
 
-    # plot_delta(2022, 'Monaco', '4', '16', 32)
-    # time_dist(2022, 'Monaco', '4', '16', 32)
+    #plot_delta(2022, 'Monaco', '16', '55', 2)
+    #time_dist(2022, 'Monaco', '16', '55', 2)
     # time_dist_all(2022, 'Monaco', drivers, 8)
 
     # ---lap 7 OCO pass VET---
     # time_dist_race_two(2022, 'Monaco', ['OCO', 'VET'], 10)
-    time_dist_race_all(2022, 'Monaco', drivers)
+    time_dist_race_all(2022, 'Monaco', ['LEC', 'SAI'], 1)
 
 
 if __name__ == '__main__':
