581e4ad02bedbc35e2709776fab3cf387ddc2254
Andrew Reeves <atreeves0821@gmail.com>
2023-04-30 10:19:23 -0400

    updated lstm to work for multiple drivers

--- tf/lstm.py
+++ tf/lstm.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,108 +15,115 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 
+def create_seq(X, lookback):
+    Xs = []
+    ys = []
+    for i in range(lookback, lookback * 2):
+        Xs.append(X[i - lookback:i])
+        ys.append(X[i])
+    return np.array(Xs), np.array(ys)
+
+
 def main():
+    train = False
+    predict = True
+
     # fix random seed for reproducibility
     np.random.seed(8)
     tf.random.set_seed(8)
 
-    df = pd.read_csv('data/__GAP1R__.csv')
-    #df = df.tail(-1)
-    df = df.drop(['Time', 'Distance_LEC', 'Distance_SAI'], axis=1)
+    df = pd.read_csv('data/__GAP2D__.csv')
+    df = df.drop(['Time'], axis=1)
+
+    #print(math.floor(len(df)/78))
 
     X = df.values
     print(X.shape)
 
-    lookback = 20
-
-    Xs = []
-    ys = []
-    for i in range(lookback, len(df)):
-        Xs.append(X[i-lookback:i])
-        ys.append(X[i])
-    X_train, y_train = np.array(Xs), np.array(ys)
+    lookback = 150
+    X_train, y_train = create_seq(X, lookback)
     print(X_train.shape)
+    print(X_train)
 
-    """#walk forward model evaluation
-    # for i in range(train_size, total_size):
-    for i in range(train_size, train_size + 2):
-        train, test = df.iloc[0:i], df.iloc[i:i + 1]
-        print('train=%d, test=%d' % (len(train), len(test)))
-        print(train)
-        print(test)
-        X_train, y_train = create_dataset(train, train.DistanceGap_SAI, time_steps)
-        X_test, y_test = create_dataset(test, test.DistanceGap_SAI, time_steps)
-        """
+    print("test start df: ", len(X_train)+2)
 
-    patience = 15
-    early_stopping = EarlyStopping(monitor='loss', patience=patience, verbose=1)
-    model_checkpoint = ModelCheckpoint('results/best_modelR.h5', monitor='loss', mode='min', verbose=1, save_best_only=True)
+    features = len(X_train[0][0])
 
-    keras.backend.clear_session()
+    if train:
+        patience = 15
+        early_stopping = EarlyStopping(monitor='loss', patience=patience, verbose=1)
+        model_checkpoint = ModelCheckpoint('results/best_model2D.h5', monitor='loss', mode='min', verbose=1, save_best_only=True)
 
-    model = Sequential()
-    model.add(LSTM(units=128, input_shape=(lookback, 1))) #, return_sequences=True))
-    #model.add(LSTM(units=2, activation='tanh'))
-    model.add(Dropout(0.5))
-    model.add(Dense(units=1, activation='linear'))
-    model.compile(
-        loss='mean_squared_error',
-        optimizer='rmsprop')
+        keras.backend.clear_session()
 
-    model.fit(
-        X_train, y_train,
-        epochs=1000,
-        batch_size=32,
-        verbose=1,
-        callbacks=[early_stopping, model_checkpoint])
+        model = Sequential()
+        model.add(LSTM(units=64, input_shape=(lookback, features))) #, return_sequences=True))
+        model.add(Dropout(0.5))
+        model.add(Dense(units=features, activation='linear'))
+        model.compile(
+            loss='mean_squared_error',
+            optimizer='rmsprop')
 
-    if os.path.exists('results/best_modelR.h5'):
-        best_model = load_model('results/best_modelR.h5')
-    else:
-        best_model = model
+        model.fit(
+            X_train, y_train,
+            epochs=1000,
+            batch_size=32,
+            verbose=1,
+            callbacks=[early_stopping, model_checkpoint])
 
-    n_predictions = 15
+    if predict:
+        best_model = load_model('results/best_model2D.h5')
 
-    # Create the input data for prediction
-    X_test = X[-lookback*2:-lookback].reshape(1, lookback, 1)
-    print(X_test)
+        print()
 
-    # Make the predictions
-    predictions = []
-    for i in range(n_predictions):
-        prediction = best_model.predict(X_test)[0][0]
-        predictions.append(prediction)
-        X_test = np.append(X_test[:, 1:, :], [[[prediction]]], axis=1)
+        n_predictions = 15
 
-    actual = X[-lookback:-lookback+n_predictions].flatten()
+        test_start = len(X_train) + 2
 
-    # Print the predictions & actual
-    print(predictions)
-    print()
-    print(actual)
+        # Create the input data for prediction
+        test_start = (len(X_train)*2) - 1
+        X_test = X[test_start:test_start+lookback].reshape(1, lookback, features)
+        print(X_test)
+        print(X_test.shape)
 
-    mse = np.sqrt(np.mean(np.square(actual-predictions)))
-    print(mse)
+        # Make the predictions
+        predictions = []
+        for i in range(n_predictions):
+            prediction = best_model.predict(X_test)
+            predictions.append(prediction.tolist())
+            #print(X_test[:, 1:, :])
+            #print(type(prediction))
+            X_test = np.append(X_test[:, 1:, :], [prediction], axis=1)
 
-    """plt.clf()
-    plt.plot(np.arange(0, len(y_train)), y_train, 'g', label="history")
-    plt.plot(np.arange(len(y_train), len(y_train) + len(y_test)), y_test, label="true")
-    plt.plot(np.arange(len(y_train), len(y_train) + len(y_test)), y_pred, 'r', label="prediction")
-    plt.ylabel('Value')
-    plt.xlabel('Time Step')
-    plt.title('Distance - LSTM')
-    plt.legend()
-    plt.savefig('images/DistanceGapR1.png')"""
+        actual = X[test_start:test_start+n_predictions]
 
-    #plt.clf()
-    plt.plot(actual, label="true")
-    plt.plot(predictions, 'r', label="prediction")
-    plt.ylabel('Value')
-    plt.xlabel('Time Step')
-    plt.title('Zoomed Distance Gap SAI - LSTM')
-    plt.legend()
-    plt.savefig('images/Zoomed_DistanceGapR.png')
-    plt.show()
+        # Flatten predictions list
+        predictions = [element for sublist in predictions for element in sublist]
+
+        # Print the predictions & actual
+        print(predictions)
+        print()
+        print(actual)
+
+        d1_true = []
+        d2_true = []
+        d1_pred = []
+        d2_pred = []
+        for i in range(len(predictions)):
+            d1_true.append(actual[i][0])
+            d2_true.append(actual[i][1])
+            d1_pred.append(predictions[i][0])
+            d2_pred.append(predictions[i][1])
+        plt.plot(d1_true, label="d1 - true")
+        plt.plot(d2_true, label="d2 - true")
+        plt.plot(d1_pred, label="d1 - prediction")
+        plt.plot(d2_pred, label="d2 - prediction")
+        plt.ylabel('Distance Gap')
+        plt.xlabel('Time Step')
+        plt.title('Zoomed Distance Gap 2D - LSTM')
+        plt.legend()
+        plt.savefig('images/Zoomed_DistanceGap2D.png')
+        plt.show()
 
 
 if __name__ == '__main__':
