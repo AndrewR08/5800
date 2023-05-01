a8822d69e3bed7bdeb2f2072801979a7e2b863d5
Andrew Reeves <atreeves0821@gmail.com>
2023-04-30 22:30:12 -0400

    added comments to main.py

--- src/main.py
+++ src/main.py
@@ -17,22 +17,27 @@ def main():
     inv_race_dict = {v: k for k, v in race_dict.items()}
     race_input = int(input("\n Select Race (1-" + str(len(keys)) + "): "))
     track = inv_race_dict[race_input]
-
     print(track)
 
+    # Set filename to save dataframe as csv
     df_filename = 'GAP_2D.csv'
 
-    circuits = get_schedule(year)
-
+    # Get user input for drivers
     ds = ['55', '4']
+
+    # Use data.py function to create distance gap csv
     #time_gap_race(year, track, drivers=ds, fn=df_filename)
 
+    # Read distance gap csv
     df = pd.read_csv('data/GAP_R.csv')
+
+    # Set model parameters for training model
     model_name = 'test_model2D'
     train_size = 0.5
     verbose = 1         #0 for no output / 1 for output
     #train(df, model_name, train_size, verbose)
 
+    # Set model parameters for predicting with model
     model_path = 'results/' + model_name + '.h5'
     # predict(model_path, n_predictions, quali_df, lookback, n_features):
 
